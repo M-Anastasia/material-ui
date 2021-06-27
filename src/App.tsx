bd43873db56b5e55fce1af84c43ce89ad5f5f3c8
mastyaeva.a <mastyaeva.a@it-universe.ru>
2021-06-27 19:42:43 +0400

    Fix theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useMuiTheme from './themes/useMuiTheme';
+import {ThemeProvider} from "@material-ui/core/styles";
+import {Button, Paper} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const theme = useMuiTheme();
+    return (
+        <ThemeProvider theme={theme}>
+            <Button variant='contained' color='secondary'>Test</Button>
+            <Paper>Test</Paper>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/themes/useMuiTheme.js
+++ src/themes/useMuiTheme.js
@@ -1,38 +1,40 @@
-import {useMemo} from "react";
 import {createMuiTheme} from '@material-ui/core/styles';
 
 const useMuiTheme = () => {
 
-    return useMemo(
-        () => {
-            createMuiTheme({
-                    palette: {
-                        primary: '#0F4780',
-                        secondary: '#6B64FF',
+    return (
+        createMuiTheme({
+                palette: {
+                    primary: {
+                        main: '#0F4780'
                     },
-                    overrides: {
-                        MuiButton: {
-                            padding: '10px 16px',
-                            contained: {
-                                color: 'secondary',
-                                "&:hover": {
-                                    backgroundColor: 'transparent',
-                                }
-                            },
-                            root: {
-                                width: '100%',
-                                fontSize: '16px',
-                                fontWeight: 'bold',
+                    secondary: {
+                        main: '#6B64FF'
+                    },
+                },
+                overrides: {
+                    MuiButton: {
+                        containedSecondary: {
+                            "&:hover": {
+                                backgroundColor: 'transparent',
+                                color: '#6B64FF',
                             }
                         },
-                        MuiPaper: {
-                            root: {
-                                boardingRadius: '8px',
-                            }
+                        root: {
+                            width: '100%',
+                            fontSize: '16px',
+                            fontWeight: 'bold',
+                            padding: '10px 16px'
+                        }
+                    },
+                    MuiPaper: {
+                        root: {
+                            boardingRadius: '8px',
                         }
                     }
                 }
-            )
-        }
-    )
+            }
+        ))
 }
+
+export default useMuiTheme;
